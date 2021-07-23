51fe003e7525d0b7eec31f5f42d30147fdcf6612
HP <mishravikas70489@gmail.com>
2021-07-23 22:38:02 +0530

    initial setup
    Change-Id: If51f6b53cfbe0fb2a579f8efda6a6b0d7e609b4c
    Signed-off-by: VMishra <mishravikas70489@gmail.com>

---
+++ base/src/main/BaseClass.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package main;
+
+/**
+ * @author HP
+ *
+ */
+public class BaseClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hey :: "+ System.currentTimeMillis());
+
+	}
+
+}
